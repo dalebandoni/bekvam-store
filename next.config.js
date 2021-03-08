eb037bbc7011f4ee55ff752a88085a52a31ca533
Dale Bandoni <dalebandoni.10@gmail.com>
2021-03-08 22:48:15 +0000

    route fix

--- next.config.js
+++ next.config.js
@@ -1,4 +1,16 @@
 module.exports = {
   // Target must be serverless
   target: 'serverless',
+  exportPathMap: async function (
+    defaultPathMap,
+    { dev, dir, outDir, distDir, buildId }
+  ) {
+    return {
+      '/': { page: '/' },
+      '/about': { page: '/about' },
+      '/p/hello-nextjs': { page: '/product/[id]' },
+      '/p/learn-nextjs': { page: '/product/[id]' },
+      '/p/deploy-nextjs': { page: '/product/[id]' },
+    }
+  },
 }
